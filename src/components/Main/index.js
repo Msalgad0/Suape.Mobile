bc280dd80f2c5132a3fa464584fd49393edb0e3f
Msalgad0 <matheus0509@live.com>
2023-12-09 11:15:29 -0300

    Add files via upload

--- src/components/Main/index.js
+++ src/components/Main/index.js
@@ -1,7 +1,6 @@
 import React from "react"
 import {View, Text} from "react-native"
-import Form from "../Form/Login"
-import Form from "../Gerador"
+import Form from '../Form/Login'
 
 export default function Main(){
     return(
